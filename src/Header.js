1d94708dfcee272873cd8be36eeb5ec1fdb9a84c
Remyfaye <igbokwefc1@gmail.com>
2022-07-04 01:25:10 +0100

    second commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+
+
+import PersonIcon from '@mui/icons-material/Person';
+export default function Header(){
+    return(
+        <div>
+
+            <PersonIcon/>
+            
+        </div>
+    )
+}
